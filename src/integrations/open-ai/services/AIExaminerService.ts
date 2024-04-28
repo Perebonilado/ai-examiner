917e4ca1475b2e99823d7a7f343801a69fd82527
perebonilado <reradiri@eminenttechnology.com>
2024-04-28 15:52:02 +0100

    changed count

--- src/integrations/open-ai/services/AIExaminerService.ts
+++ src/integrations/open-ai/services/AIExaminerService.ts
@@ -13,7 +13,7 @@ export class AIExaminerService {
   private openAiClient: OpenAI;
   private examiner: OpenAI.Beta.Assistants.Assistant;
   private prompt = `
-  Based off the file, generate 10 multiple choice questions and return only a json array format like this: [{ id: string, question: string, options: { value: string, id: string }[], correctAnswerId: string, explanation: string}]. This json structure should be the only thing you return, no other strings whatsoever`;
+  Based off the file, generate 5 multiple choice questions and return only a json array format like this: [{ id: string, question: string, options: { value: string, id: string }[], correctAnswerId: string, explanation: string}]. This json structure should be the only thing you return, no other strings whatsoever`;
 
   constructor() {
     this.intializeOpenAiClient();
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1')
   app.enableCors();
-  // const config = new DocumentBuilder()
-  //   .setTitle('Examiner AI')
-  //   .setDescription(
-  //     `The examiner api integrates with open AI's assistant help students study and prepare for their exams better`,
-  //   )
-  //   .setVersion('1.0')
-  //   .addTag('ai')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
