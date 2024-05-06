e775e88f76bd9c81d047bc19a50ba16b2a7146d4
perebonilado <reradiri@eminenttechnology.com>
2024-05-06 19:02:24 +0100

    create course handler

--- src/infra/web/controllers/CourseController.ts
+++ src/infra/web/controllers/CourseController.ts
@@ -1,13 +1,17 @@
 import {
+  Body,
   Controller,
   Get,
   HttpException,
   HttpStatus,
   Inject,
+  Post,
   Req,
   UseGuards,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { CreateCourseHandler } from 'src/business/handlers/Course/CreateCourseHandler';
+import { CreateCourseDto } from 'src/dto/CreateCourseDto';
 import { AuthGuard } from 'src/infra/auth/guards/AuthGuard';
 import { VerifiedTokenModel } from 'src/infra/auth/models/VerifiedTokenModel';
 import { CourseQueryService } from 'src/query/services/CourseQueryService';
@@ -16,6 +20,8 @@ import { CourseQueryService } from 'src/query/services/CourseQueryService';
 export class CourseController {
   constructor(
     @Inject(CourseQueryService) private courseQueryService: CourseQueryService,
+    @Inject(CreateCourseHandler)
+    private createCourseHandler: CreateCourseHandler,
   ) {}
 
   @UseGuards(AuthGuard)
@@ -27,6 +33,25 @@ export class CourseController {
     } catch (error) {
       throw new HttpException(
         error?.response ?? 'Failed to find user courses',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('')
+  public async createCourse(
+    @Req() request: Request,
+    @Body() body: Omit<CreateCourseDto, 'userId'>,
+  ) {
+    try {
+      const userToken = request['user'] as VerifiedTokenModel;
+      return await this.createCourseHandler.handle({
+        payload: { ...body, userId: userToken.sub },
+      });
+    } catch (error) {
+      throw new HttpException(
+        error?.response ?? 'Failed to create course',
         HttpStatus.BAD_REQUEST,
       );
     }
