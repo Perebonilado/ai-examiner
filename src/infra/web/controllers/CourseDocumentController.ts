2af2950ec03928549a6f40d04e31949542e19331
perebonilado <reradiri@eminenttechnology.com>
2024-05-13 14:23:07 +0100

    changed endpoint to diff controller

--- src/infra/web/controllers/CourseDocumentController.ts
+++ src/infra/web/controllers/CourseDocumentController.ts
@@ -10,7 +10,6 @@ import {
   HttpException,
   HttpStatus,
   Get,
-  Param,
   Query,
   ParseIntPipe,
 } from '@nestjs/common';
@@ -21,12 +20,9 @@ import { Request } from 'express';
 import { VerifiedTokenModel } from 'src/infra/auth/models/VerifiedTokenModel';
 import { CreateCourseDocumentDto } from 'src/dto/CreateCourseDocumentDto';
 import { CourseDocumentQueryService } from 'src/query/services/CourseDocumentQueryService';
-import { GenerateCourseDocumentQuestionDto } from 'src/dto/GenerateCourseDocumentQuestionsDto';
 import { CreateQuestionHandler } from 'src/business/handlers/Question/CreateQuestionHandler';
 import { ExaminerService } from 'src/integrations/open-ai/services/ExaminerService';
-import { extractQuestionsFromMessages } from 'src/utils';
-import { EnvironmentVariables } from 'src/EnvironmentVariables';
-import { initialGenerationPrompt } from 'src/constants';
+
 
 @Controller('course-document')
 export class CourseDocumentController {
@@ -69,83 +65,6 @@ export class CourseDocumentController {
     } catch (error) {
       throw new HttpException(
         error?.response ?? 'Failed to find course documents',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-  }
-
-  @UseGuards(AuthGuard)
-  @Post('/:id/generate-questions')
-  public async generateDocumentQuestions(
-    @Param() params: GenerateCourseDocumentQuestionDto,
-    @Req() request: Request,
-  ) {
-    try {
-      const userToken = request['user'] as VerifiedTokenModel;
-      const document =
-        await this.courseDocumentQueryService.findCourseDocumentById(
-          params.id,
-          userToken.sub,
-        );
-
-      if (document) {
-        const existingThread = await this.examinerService.findThread(
-          document.openAiThreadId,
-        );
-
-        // check if vector store has expired, if so:
-        // create new store, attach file and attach to thread
-        if (
-          !existingThread.tool_resources.file_search.vector_store_ids.length
-        ) {
-          const newVectorStore = await this.examinerService.createVectorStore(
-            document.title,
-          );
-          const updatedVectorStore =
-            await this.examinerService.attachFileToVectorStore(
-              document.openAiFileId,
-              newVectorStore.id,
-            );
-
-          await this.examinerService.attachVectorStoreToThread(
-            existingThread.id,
-            updatedVectorStore.id,
-          );
-        }
-
-        await this.examinerService.createThreadMessage(
-          existingThread.id,
-          initialGenerationPrompt,
-        );
-
-        await this.examinerService.createRun(
-          EnvironmentVariables.config.assistantId,
-          existingThread.id,
-        );
-
-        const messages = await this.examinerService.retrieveThreadMessages(
-          existingThread.id,
-        );
-
-        const mostRecentlyGeneratedQuestions =
-          extractQuestionsFromMessages(messages);
-
-        return await this.createQuestionHandler.handle({
-          payload: {
-            courseDocumentId: document.id,
-            data: mostRecentlyGeneratedQuestions,
-            userId: userToken.sub,
-          },
-        });
-      } else {
-        throw new HttpException(
-          'Course Document does not exist',
-          HttpStatus.NOT_FOUND,
-        );
-      }
-    } catch (error) {
-      throw new HttpException(
-        error?.response ?? 'Failed to generate questions for document',
         HttpStatus.BAD_REQUEST,
       );
     }
--- src/infra/web/controllers/QuestionsController.ts
+++ src/infra/web/controllers/QuestionsController.ts
@@ -10,6 +10,7 @@ import {
   Param,
   Query,
   ParseIntPipe,
+  Post,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/infra/auth/guards/AuthGuard';
 import { Request } from 'express';
@@ -17,12 +18,24 @@ import { VerifiedTokenModel } from 'src/infra/auth/models/VerifiedTokenModel';
 import { GetAllQuestionsDto } from 'src/dto/GetAllQuestionsDto';
 import { QuestionQueryService } from 'src/query/services/QuestionQueryService';
 import { GetQuestionByIdDto } from 'src/dto/GetQuestionByIdDto';
+import { extractQuestionsFromMessages } from 'src/utils';
+import { EnvironmentVariables } from 'src/EnvironmentVariables';
+import { initialGenerationPrompt } from 'src/constants';
+import { CourseDocumentQueryService } from 'src/query/services/CourseDocumentQueryService';
+import { ExaminerService } from 'src/integrations/open-ai/services/ExaminerService';
+import { CreateQuestionHandler } from 'src/business/handlers/Question/CreateQuestionHandler';
+import { GenerateCourseDocumentQuestionDto } from 'src/dto/GenerateCourseDocumentQuestionsDto';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(
     @Inject(QuestionQueryService)
     private questionQueryService: QuestionQueryService,
+    @Inject(CourseDocumentQueryService)
+    private courseDocumentQueryService: CourseDocumentQueryService,
+    @Inject(ExaminerService) private examinerService: ExaminerService,
+    @Inject(CreateQuestionHandler)
+    private createQuestionHandler: CreateQuestionHandler,
   ) {}
 
   @UseGuards(AuthGuard)
@@ -40,6 +53,84 @@ export class QuestionsController {
     } catch (error) {
       throw new HttpException(
         error?.response ?? 'Failed to find question by id ' + params.id,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  //id refers to the course document id
+  @UseGuards(AuthGuard)
+  @Post('/:id/generate-questions')
+  public async generateDocumentQuestions(
+    @Param() params: GenerateCourseDocumentQuestionDto,
+    @Req() request: Request,
+  ) {
+    try {
+      const userToken = request['user'] as VerifiedTokenModel;
+      const document =
+        await this.courseDocumentQueryService.findCourseDocumentById(
+          params.id,
+          userToken.sub,
+        );
+
+      if (document) {
+        const existingThread = await this.examinerService.findThread(
+          document.openAiThreadId,
+        );
+
+        // check if vector store has expired, if so:
+        // create new store, attach file and attach to thread
+        if (
+          !existingThread.tool_resources.file_search.vector_store_ids.length
+        ) {
+          const newVectorStore = await this.examinerService.createVectorStore(
+            document.title,
+          );
+          const updatedVectorStore =
+            await this.examinerService.attachFileToVectorStore(
+              document.openAiFileId,
+              newVectorStore.id,
+            );
+
+          await this.examinerService.attachVectorStoreToThread(
+            existingThread.id,
+            updatedVectorStore.id,
+          );
+        }
+
+        await this.examinerService.createThreadMessage(
+          existingThread.id,
+          initialGenerationPrompt,
+        );
+
+        await this.examinerService.createRun(
+          EnvironmentVariables.config.assistantId,
+          existingThread.id,
+        );
+
+        const messages = await this.examinerService.retrieveThreadMessages(
+          existingThread.id,
+        );
+
+        const mostRecentlyGeneratedQuestions =
+          extractQuestionsFromMessages(messages);
+
+        return await this.createQuestionHandler.handle({
+          payload: {
+            courseDocumentId: document.id,
+            data: mostRecentlyGeneratedQuestions,
+            userId: userToken.sub,
+          },
+        });
+      } else {
+        throw new HttpException(
+          'Course Document does not exist',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+    } catch (error) {
+      throw new HttpException(
+        error?.response ?? 'Failed to generate questions for document',
         HttpStatus.BAD_REQUEST,
       );
     }
