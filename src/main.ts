fdce80c99ee64799d5547c253bd807f789f43ba9
perebonilado <reradiri@eminenttechnology.com>
2024-05-21 01:57:45 +0100

    slash

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('/api/v1');
   app.use(cookieParser());
   app.enableCors();
   await app.listen(EnvironmentVariables.config.port || 3000, '0.0.0.0', () => {
