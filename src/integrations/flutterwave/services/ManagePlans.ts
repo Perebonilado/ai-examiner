5a4ca714559f124710e053fa144312dd84299b8f
perebonilado <reradiri@eminenttechnology.com>
2024-05-27 08:19:38 +0100

    get plans endpoint integrated

--- src/integrations/flutterwave/services/ManagePlans.ts
+++ src/integrations/flutterwave/services/ManagePlans.ts
@@ -2,10 +2,37 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { EnvironmentVariables } from 'src/EnvironmentVariables';
 import { AxiosResponse } from 'axios';
+import {
+  GetPaymentPlansQueryModel,
+  GetPaymentPlansResponseModel,
+} from '../models/PaymentPlansModel';
 
 @Injectable()
 export class ManagePlans {
-    constructor(private httpService: HttpService){}
+  constructor(private httpService: HttpService) {}
 
-    baseUrl = 'https://api.flutterwave.com/v3';
+  baseUrl = 'https://api.flutterwave.com/v3';
+
+  public async getPaymentPlants(query: GetPaymentPlansQueryModel) {
+    try {
+      const url = `${this.baseUrl}/payment-plans`;
+
+      const { data } = await this.httpService.axiosRef.get<
+        any,
+        AxiosResponse<GetPaymentPlansResponseModel>
+      >(url, {
+        params: query,
+        headers: {
+          Authorization: `Bearer ${EnvironmentVariables.config.flutterwaveSecretKey}`,
+        },
+      });
+
+      return data.data;
+    } catch (error) {
+      throw new HttpException(
+        'An error occured while trying to find subscription',
+        HttpStatus.BAD_GATEWAY,
+      );
+    }
+  }
 }
