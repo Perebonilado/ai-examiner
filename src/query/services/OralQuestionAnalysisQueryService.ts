5113a1537fb712ea6f193b9b6b277e3442f2f9c2
perebonilado <eradiribonnie@gmail.com>
2025-03-04 07:39:35 +0100

    viva done

--- src/query/services/OralQuestionAnalysisQueryService.ts
+++ src/query/services/OralQuestionAnalysisQueryService.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Op, QueryTypes } from 'sequelize';
 import QueryError from 'src/error-handlers/query/QueryError';
 import { OralQuestionAnalysisModel } from 'src/infra/db/models/OralQuestionAnalysisModel';
+import { QuestionModel } from 'src/infra/db/models/QuestionModel';
+import * as moment from 'moment';
 
 @Injectable()
 export class OralQuestionAnalysisQueryService {
@@ -11,6 +14,34 @@ export class OralQuestionAnalysisQueryService {
       throw new QueryError(
         'Failed to find oral question analysis by question id ' + questionId,
       );
+    }
+  }
+
+  public async countOralQuestionsGenratedForCurrentMonth(userId: string) {
+    try {
+      const startOfMonth = moment.utc(new Date()).startOf('month').toDate();
+      const endOfMonth = moment.utc(new Date()).endOf('month').toDate();
+      const data = (await OralQuestionAnalysisModel.sequelize.query(
+        `
+        SELECT * 
+      FROM oral_question_analysis AS orq
+      INNER JOIN question q ON orq.question_id = q.id
+      WHERE q.user_id = :userId
+      AND q.created_on BETWEEN :startOfMonth AND :endOfMonth;
+
+        `,
+        {
+          replacements: { userId, startOfMonth, endOfMonth },
+          type: QueryTypes.SELECT,
+        },
+      )) as unknown as any[];
+
+      return data.length
+    } catch (error) {
+      console.log(error);
+      throw new QueryError(
+        'Failed to count viva questions for month',
+      ).InnerError(error);
     }
   }
 }
