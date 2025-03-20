c24f41f0b247d264e17cd24952306bcdbf0dd65e
perebonilado <eradiribonnie@gmail.com>
2025-03-20 10:59:40 +0100

    update call credit

---
+++ src/business/handlers/CallCredits/CreateCallCreditsHandler.ts
@@ -0,0 +1,50 @@
+import { Injectable, Inject, HttpStatus } from '@nestjs/common';
+import AbstractRequestHandlerTemplate from '../AbstractRequestHandlerTemplate';
+import { CreateCallCreditsRequest } from '../request/CreateCallCreditsRequest';
+import { CreateCallCreditsResponse } from '../response/CreateCallCreditsResponse';
+import { CommandResponse } from '../response/CommandResponse';
+import { HandlerError } from 'src/error-handlers/business/HandlerError';
+import { CallCreditsRepository } from 'src/business/repository/CallCreditsRepository';
+import { CallCreditsModel } from 'src/infra/db/models/CallCreditsModel';
+
+@Injectable()
+export class CreateCallCreditsHandler extends AbstractRequestHandlerTemplate<
+  CreateCallCreditsRequest,
+  CreateCallCreditsResponse
+> {
+  constructor(
+    @Inject(CallCreditsRepository)
+    private callCreditsRepository: CallCreditsRepository,
+  ) {
+    super();
+  }
+
+  protected async handleRequest(
+    request: CreateCallCreditsRequest,
+  ): Promise<CommandResponse<CreateCallCreditsResponse>> {
+    try {
+      const { timeToAddMs, userId } = request;
+      const callCreditsPayload = {
+        userId,
+        remainingTimeMs: timeToAddMs,
+        totalTimePurchasedMs: 0,
+      } as CallCreditsModel;
+
+      const createdCredits =
+        await this.callCreditsRepository.create(callCreditsPayload);
+
+      return {
+        data: {
+          id: createdCredits.id,
+          reaminingCredits: createdCredits.remainingTimeMs,
+        },
+        message: 'Call Credits Created',
+        status: HttpStatus.CREATED,
+      };
+    } catch (error) {
+      throw new HandlerError(
+        'Failed to handle call credit creation request',
+      ).InnerError(error);
+    }
+  }
+}
