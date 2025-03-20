eaf2c0e8a9891a61cbb1fe09dddf3a3484b0ef6c
perebonilado <eradiribonnie@gmail.com>
2025-03-20 12:32:10 +0100

    credit cycle implemented on server

--- src/webhooks/web/VapiWebHook.ts
+++ src/webhooks/web/VapiWebHook.ts
@@ -10,12 +10,17 @@ import {
 import { VapiCallEndedDto } from '../dto/VapiCallEndedDto';
 import { Response } from 'express';
 import { CreateOralQuestionAnalysisHandler } from 'src/business/handlers/OralQuestionAnalysis/CreateOralQuestionAnalysisHandler';
+import { UpdateCallCreditsHandler } from 'src/business/handlers/CallCredits/UpdateCallCreditsHandler';
+import { UserQueryService } from 'src/query/services/UserQueryService';
 
 @Controller('webhook/vapi')
 export class VapiWebhook {
   constructor(
     @Inject(CreateOralQuestionAnalysisHandler)
     private createOralQuestionAnalysisHandler: CreateOralQuestionAnalysisHandler,
+    @Inject(UpdateCallCreditsHandler)
+    private updateCallCreditHandler: UpdateCallCreditsHandler,
+    @Inject(UserQueryService) private userQueryService: UserQueryService,
   ) {}
 
   @Post('')
@@ -26,9 +31,16 @@ export class VapiWebhook {
     // acknowledge
     response.sendStatus(200);
 
-    
     try {
       if (body.message.type === 'end-of-call-report') {
+        const { customerEmail } = body.message.assistant
+          .metadata as unknown as any;
+        const user = await this.userQueryService.findOne(customerEmail);
+        await this.updateCallCreditHandler.handle({
+          action: 'subtract_remaining_time',
+          timeToUpdate: body.message.durationMs,
+          userId: user.id,
+        });
         await this.createOralQuestionAnalysisHandler.handle({ data: body });
       }
     } catch (error) {
