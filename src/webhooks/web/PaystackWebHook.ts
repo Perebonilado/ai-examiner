cde3c970310395d5fdbf9a5a35e669f6a7a7edb1
perebonilado <eradiribonnie@gmail.com>
2025-07-25 15:36:02 +0100

    increased to two hours for US region

--- src/webhooks/web/PaystackWebHook.ts
+++ src/webhooks/web/PaystackWebHook.ts
@@ -72,7 +72,7 @@ export class PaystackWebhook {
                 if (
                   (planDescription['region'] as string)
                     .toLowerCase()
-                    .includes('africa')
+                    .includes('america')
                 ) {
                   const callCredits =
                     await this.callCreditsQueryService.findByUserId(user.id);
